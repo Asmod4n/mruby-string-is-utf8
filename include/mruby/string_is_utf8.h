1c639fe845008d437420a61852329a158ddd74b1
Asmod4n <Asmod4n@users.noreply.github.com>
2017-04-14 13:12:11 +0200

    fix compiling with c++

--- include/mruby/string_is_utf8.h
+++ include/mruby/string_is_utf8.h
@@ -4,7 +4,11 @@
 #include <mruby.h>
 #include <mruby/string.h>
 
+MRB_BEGIN_DECL
+
 MRB_API mrb_bool mrb_str_is_utf8(mrb_value str);
 MRB_API mrb_int mrb_str_is_utf8_raw(mrb_value str);
+
+MRB_END_DECL
 
 #endif
